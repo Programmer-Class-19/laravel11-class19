2cb32e122d7f0a99e180d4b6dbaa4887b46f4521
unknown <masuduciha@gmail.com>
2024-09-27 18:45:25 +0700

    Jum.at

--- Masud/laravel11repo/resources/views/components/navbar.blade.php
+++ Masud/laravel11repo/resources/views/components/navbar.blade.php
@@ -8,10 +8,10 @@
           <div class="hidden md:block">
             <div class="ml-10 flex items-baseline space-x-4">
               <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-              <a href="/" class="{{ request()->is('/') ? 'bg-gray-900 px-3 py-2 text-sm fontet-medium text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'}} rounded-md " aria-current="page">Home</a>
-              <a href="/blog" class="{{ request()->is('blog') ? 'bg-gray-900 px-3 py-2 text-sm fontet-medium text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'}} rounded-md px-3 py-2 text-sm font-medium">Blog</a>
-              <a href="/about" class="{{ request()->is('about') ? 'bg-gray-900 px-3 py-2 text-sm fontet-medium text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'}} rounded-md px-3 py-2 text-sm font-medium">About</a>
-              <a href="/contact" class=" {{ request()->is('contact') ? 'bg-gray-900 px-3 py-2 text-sm fontet-medium text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'}} rounded-md px-3 py-2 text-sm font-medium">Contact</a>
+              <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>              
+              <x-nav-link href="blog" :active="request()->is('blog')">Blog</x-nav-link>              
+              <x-nav-link href="about" :active="request()->is('about')">About</x-nav-link>              
+              <x-nav-link href="contact" :active="request()->is('contact')">Contact</x-nav-link>                           
             </div>
           </div>
         </div>
