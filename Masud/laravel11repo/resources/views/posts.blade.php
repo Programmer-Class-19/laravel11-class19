f8e3d61d75b0fb7ff14078436c0e897f0331caf2
unknown <masuduciha@gmail.com>
2024-09-28 14:55:59 +0700

    sabtu

---
+++ Masud/laravel11repo/resources/views/posts.blade.php
@@ -0,0 +1,23 @@
+<x-layout>
+<x-slot:title>{{ $title }}</x-slot:title>
+ 
+
+   @foreach ( $posts as $post) 
+
+    <article class="py-8 max-w-screen-md border-b border-gray-300">
+       <a href="/posts/ {{ $post['id'] }}" class=" hover:underline">
+           <h2 class="mb-3 text-3xl tracking-tight font-bold text-gray-900">{{ $post ['title'] }}</h2>
+       </a> 
+        <div class="text-base text-gray-500">
+            <a href="#">{{ $post ['author'] }}</a> | 1 Oktober 2004
+
+        </div>
+
+        <p class="my-4 font-light">{{ Str::limit( $post ['body'],150 ) }}</p>
+         
+        <a href="/posts/{{ $post['id'] }}" class="font-medium text-blue-500 hover:underline">Read more &raquo; </a>
+
+    </article>
+   @endforeach
+
+</x-layout> 
--- Masud/laravel11repo/routes/web.php
+++ Masud/laravel11repo/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -7,14 +8,51 @@ Route::get('/', function () {
 });
 
 Route::get('/about', function () {
-    return view('about', [ 'nama' => "Mas'ud" ], [ 'title' => 'About'] );
+    return view('about', [ 'nama' => "Mas'ud", 'title' => 'About'] );
 });
 
 
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog']);
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog', 'posts' => [
+        [
+            'id' => 1,
+            'title' => 'Judul Artikel 1',
+            'author' => "Mas'ud Maungzyy",
+            'body' => '    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi omnis dolor optio beatae magni, minus facilis laudantium est delectus inventore quos necessitatibus quis nostrum explicabo ipsum labore. Sunt, vitae repudiandae!'
+        ],
+        [
+            'id' => 2,
+            'title' => 'Judul Artikel 2',
+            'author' => "Mas'ud Maungzyy",
+            'body' => '    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt alias sunt architecto voluptate pariatur quas recusandae doloribus, excepturi cupiditate. Dolore ad doloremque aspernatur ipsa est sed? Dolor dolores beatae praesentium.'
+        ]
+        
+    ]]);
 });
+
+Route::get('/posts/{id}', function($id) {
+    $posts = [
+        [
+            [
+                'id' => 1,
+                'title' => 'Judul Artikel 1',
+                'author' => "Mas'ud Maungzyy",
+                'body' => '    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi omnis dolor optio beatae magni, minus facilis laudantium est delectus inventore quos necessitatibus quis nostrum explicabo ipsum labore. Sunt, vitae repudiandae!'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Judul Artikel 2',
+                'author' => "Mas'ud Maungzyy",
+                'body' => '    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt alias sunt architecto voluptate pariatur quas recusandae doloribus, excepturi cupiditate. Dolore ad doloremque aspernatur ipsa est sed? Dolor dolores beatae praesentium.'
+            ]
+            
+        
+    ]];
+    
+    $post = Arr::first();
+});
+
 
 Route::get('/contact', function () {
     return view('contact', ['title' => 'Contact']);
